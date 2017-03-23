ebd0d614eea03900777d97dde2d00a6874b2bf60
JaeYong.Kim <skyroon25@naver.com>
2017-03-23 21:10:33 +0900

    뮤직플레이어 플레이리스트 완성 다음에는 이미지 추가하면 됨

--- app/src/main/java/com/musicplayerjykim/musicplayerapp/Fragment/SongFragment.java
+++ app/src/main/java/com/musicplayerjykim/musicplayerapp/Fragment/SongFragment.java
@@ -1,19 +1,26 @@
 package com.musicplayerjykim.musicplayerapp.Fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.musicplayerjykim.musicplayerapp.R;
 
 /**
@@ -24,6 +31,8 @@ public class SongFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     private ListView mListView;
+
+    String imageFile;
 
     public SongFragment() {
 
@@ -57,16 +66,58 @@ public class SongFragment extends Fragment {
 
         mListView = (ListView) view.findViewById(R.id.playlist);
 
-        Cursor cursor = getActivity().getContentResolver().query(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                null
-        );
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        MusicCursorAdapter adapter = new MusicCursorAdapter (getActivity(), cursor);
-        mListView.setAdapter(adapter);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1000);
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1000);
+            }
+        } else {
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case 1000: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Cursor cursor = getActivity().getContentResolver().query(
+                            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            null
+                    );
+
+                    MusicCursorAdapter adapter = new MusicCursorAdapter (getActivity(), cursor);
+                    mListView.setAdapter(adapter);
+
+                } else {
+
+                    getActivity().finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
 
     }
 
@@ -92,7 +143,7 @@ public class SongFragment extends Fragment {
             void onFragmentInteraction(Uri uri);
         }
 
-    private static class MusicCursorAdapter extends CursorAdapter {
+    private class MusicCursorAdapter extends CursorAdapter {
 
         public MusicCursorAdapter(Context context, Cursor c) {
             super(context, c, false);
@@ -105,7 +156,7 @@ public class SongFragment extends Fragment {
 
             viewHolder.albumArtist = (TextView)convertView.findViewById(R.id.song_artist_text);
             viewHolder.albumTitle = (TextView)convertView.findViewById(R.id.song_title_text);
-//            viewHolder.albumImage = (ImageView)convertView.findViewById(R.id.song_image);
+            viewHolder.albumImage = (ImageView)convertView.findViewById(R.id.song_image);
 
             convertView.setTag(viewHolder);
 
@@ -117,12 +168,26 @@ public class SongFragment extends Fragment {
             final ViewHolder viewHolder = (ViewHolder) view.getTag();
             viewHolder.albumArtist.setText(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)));
             viewHolder.albumTitle.setText(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)));
-//            viewHolder.albumImage.setImageResource(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-//            Glide.with(context).load(path).into(viewHolder.albumImage);
+            Uri uri = Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)));
+            Glide.with(SongFragment.this).load(getRealPathFromURI(uri)).into(viewHolder.albumImage);
+        }
+
+        private String getRealPathFromURI(Uri contentURI) {
+            String result;
+            Cursor cursor = getContext().getContentResolver().query(contentURI, null, null, null, null);
+            if (cursor == null) {
+                result = contentURI.getPath();
+            } else {
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+                result = cursor.getString(idx);
+                cursor.close();
+            }
+            return result;
         }
 
         private class ViewHolder {
-//            ImageView albumImage;
+            ImageView albumImage;
             TextView albumArtist;
             TextView albumTitle;
         }
